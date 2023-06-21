1988fc0926ccf1995ed2d8c167017197af5d06ce
Jon Haitz Legarreta Gorroño <jon.haitz.legarreta@gmail.com>
2023-06-21 09:36:37 -0400

    STYLE: Adjust institution column count
    Adjust institution column count following the removal of MGH in commit
    80a4575057d2c283c94a65ccb12a05e614de9e45.

--- src/components/institutions/Institutions.js
+++ src/components/institutions/Institutions.js
@@ -9,7 +9,7 @@ const Institutions = () => {
             <div className="flex justify-center items-center text-center">
                 <Title title='Hosting researchers across Boston' body='Institutions' />
             </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4 xl:gap-10">
+            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 xl:gap-10">
                 {institutions.map((item, index) => (
                     <Card
                         title={item.title}
