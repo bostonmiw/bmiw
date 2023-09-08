f251fd44755dca03370350426f565c270a3fc6fe
Jon Haitz Legarreta Gorroño <jon.haitz.legarreta@gmail.com>
2023-09-08 14:27:56 -0400

    ENH: Add venue details
    Add venue details.

--- src/components/venue/Venue.js
+++ src/components/venue/Venue.js
@@ -10,13 +10,13 @@ const Venue = () => {
             <div className='w-full'>
                 <div className="w-full h-auto flex flex-col lgl:flex-row justify-between gap-6 lgl:gap-0">
                     <span className='text-base text-gray-400 tracking-wide'>
-                        Exact location TBD
+                        114 16th St, Charlestown
                         <br/>
-                        Boston, MA USA
+                        MA 02129 USA
                     </span>
                     <iframe className="directions"
                             title="direction"
-                            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11792.685101158833!2d-71.0638692!3d42.360189!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89e370866bb028a7%3A0xc2f8ab58860405!2sFaneuil%20Hall%20Marketplace!5e0!3m2!1sen!2sus!4v1685319003214!5m2!1sen!2sus"></iframe>
+                            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12235.061279382558!2d-71.06558908006292!3d42.374556622499654!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89e370f7f468f54f%3A0xae2e2ea8a4fb104d!2s114%2016th%20St%2C%20Bunker%20Hill%2C%20MA%2002129!5e0!3m2!1sen!2sus!4v1694200400166!5m2!1sen!2sus"></iframe>
                 </div>
             </div>
         </section>
