fea9a7ee3b2be523c6b28f399cad25773b228546
Jon Haitz Legarreta Gorroño <jon.haitz.legarreta@gmail.com>
2023-10-25 10:30:59 -0400

    ENH: Add registration requirement notice
    Make it explicit that registration to the event is required: change the
    inconsequential "Find us in Boston" message for the "Registration
    required" message.

--- src/components/banner/Media.js
+++ src/components/banner/Media.js
@@ -5,7 +5,7 @@ const Media = () => {
         <div className="flex flex-col xl:flex-row gap-6 lgl:gap-0 justify-between items-center lgl:items-start">
             <div>
                 <h2 className="text-base uppercase font-titleFont mb-4 text-center lgl:text-left">
-                    Find us in Boston
+                    Registration required
                 </h2>
             </div>
         </div>
